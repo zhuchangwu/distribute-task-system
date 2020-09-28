5492b3dd541835840046a39fe47e1c56e31fea43
zhuchangwu_dxm <zhuchangwu@duxiaoman.com>
2020-09-28 11:04:28 +0800

    通用常量、函数

--- master/common/Protocol.go
+++ master/common/Protocol.go
@@ -1,1 +1,63 @@
 package common
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+var (
+	//定制任务
+	CronJobNamePrefix = "/cron/jobs/"
+	KillCronJobNamePrefix = "/cron/killer/"
+)
+
+// 任务的封装结构体
+type Job struct {
+	Name     string `json:"name"`
+	Command  string `json:"command"`
+	CronExpr string `json:"cronExpr"`
+}
+
+// 返回给前端的相应体封装结构体
+type ResResult struct {
+	Code int         `json:"code"`
+	Msg  string      `json:"msg"`
+	Data interface{} `json:"data"`
+}
+
+// 构建成功的返回值
+func BuildSuccessRes(msg string, data interface{}) (resByte []byte, err error) {
+	var (
+		res *ResResult
+	)
+	res = &ResResult{
+		Code: 200,
+		Msg:  msg,
+		Data: data,
+	}
+
+	if resByte, err = json.Marshal(res); err != nil {
+		fmt.Printf("BuildSuccessRes json.Marshal(res) err=[%v]", err)
+		return nil, err
+	}
+	return
+}
+
+// 构建失败的返回值
+func BuildFailRes(msg string, data interface{}) (resByte []byte, err error) {
+
+	var (
+		res *ResResult
+	)
+	res = &ResResult{
+		Code: 500,
+		Msg:  msg,
+		Data: data,
+	}
+
+	if resByte, err = json.Marshal(res); err != nil {
+		fmt.Printf("BuildSuccessRes json.Marshal(res) err=[%v]", err)
+		return nil, err
+	}
+	return
+}
